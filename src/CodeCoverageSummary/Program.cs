62f89956ad68d5e98a8269ab26cf9b0fcec02cae
pocketnest-erik <erik@pocketnest.com>
2025-06-20 11:59:22 -0500

    wip : ,

--- src/CodeCoverageSummary/Program.cs
+++ src/CodeCoverageSummary/Program.cs
@@ -45,7 +45,7 @@ namespace CodeCoverageSummary
                                          //if limiting to PR files, get list now
                                          // if (o.PRFilesOnly)
                                          // {
-                                         Console.WriteLine(o);
+                                         Console.WriteLine(o.PRFilesString);
                                          
                                          return 0;
                                          // }
